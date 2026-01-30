3bb265f9655fbc9821c396b9da7e278d9ddfaf49
KenyaEcampaign <kenya.ecampaign@gmail.com>
2026-01-30 19:08:59 +0300

    Fix email with SendGrid

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,15 +3,13 @@ import sgMail from "@sendgrid/mail";
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export async function sendEmail(to, subject, html) {
-  try {
-    await sgMail.send({
-      to,
-      from: process.env.MAIL_FROM,
-      subject,
-      html,
-    });
-  } catch (err) {
-    console.error("Email error:", err.response?.body || err.message);
-    throw new Error("Email failed");
-  }
+  await sgMail.send({
+    to,
+    from: {
+      email: process.env.EMAIL_FROM,
+      name: "Kenya E-Campaign 🇰🇪"
+    },
+    subject,
+    html
+  });
 }
